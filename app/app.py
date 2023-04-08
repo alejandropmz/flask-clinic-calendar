eceb1a465cdda57d91d0e7c4f29f9fbe77beccf0
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-08 16:09:20 -0500

    facturas y listado

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime, time
 
 app = Flask(__name__)
 
@@ -16,15 +17,27 @@ def pacientes():
 
 @app.route("/citas")
 def citas():
-    return "citas"
+    citas = [
+        (1, "alejandro pacheco", (9,00), (9, 45), "control", True),
+        (2, "alex pacheco", (10, 15), (11,00), "vacuna", False)
+    ]
+    print(citas)
+    hours = citas[0][2]
+    return render_template('citas.html', appointments=citas)
 
 @app.route("/facturas")
 def facturas():
-    return "facturas"
+    facturas = [
+        ("Alex Pacheco", 122, (2023, 8, 3), 211000, 12, (2023, 8, 4), True),
+        ("Alejandro Pacheco", 126, (2023, 6, 4), 92000, 12, (2023, 7, 5), False),
+        ("Maria Hernandez", 128, (2023, 8, 4), 526500, 12, (2023, 8, 5), True)
+    ]
+    print(facturas)
+    return render_template('facturas.html', checks=facturas)
 
 @app.route("/listado")
 def listado():
-    return "listado"
+    return render_template('listado.html')
 
 @app.route("/sesion")
 def sesion():
