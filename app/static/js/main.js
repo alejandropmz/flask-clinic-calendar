7842411d16690356002e6fe955381c83be67114b
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-19 21:03:52 -0500

    validacion para eliminacion de registro mediante alert

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,10 @@
-const eliminarPaciente = document.getElementById("")
+document.addEventListener("DOMContentLoaded", function () {
+  const eliminar = document.getElementById("boton-eliminar");
+
+  eliminar.addEventListener("click", (e) => {
+    if (!confirm("¿Estás seguro de querer eliminar el registro?")) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+});
